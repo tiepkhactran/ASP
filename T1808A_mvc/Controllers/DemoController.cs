0d64fd3255eec0f62bf730e13725535f81e9452a
tiepkhactran <tiepkhactran@users.noreply.github.com>
2019-11-12 16:40:18 +0700

    ASP.NET

---
+++ T1808A_mvc/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T1808A_mvc.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ T1808A_mvc/Controllers/MemberController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T1808A_mvc.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult Register()
+        {
+            
+            return View();
+        }
+        public ActionResult Save(string name,int age)
+        {
+            ViewBag.name = name;
+            ViewBag.age = age;
+
+            return View();
+        }
+    }
+}
